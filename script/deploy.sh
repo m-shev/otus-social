0a7cb8c3a77cc1bd2b17598df2b5253306e9ae26
mshev <mshev@list.ru>
2021-10-26 20:38:22 +0300

    deploy script

---
+++ script/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+server=sigma@77.244.214.34
+imagesPath=/home/sigma/images
+
+scp -P 5544 ./images/sigma-nginx.tar $server:$imagesPath
+scp -P 5544 ./images/sigma-social.tar $server:$imagesPath
+scp -P 5544 ./images/sigma-social-db.tar $server:$imagesPath
+
+ssh -p 5544 $server docker load -i $imagesPath/sigma-nginx.tar
+ssh -p 5544 $server docker load -i $imagesPath/sigma-social.tar
+ssh -p 5544 $server docker load -i $imagesPath/sigma-nginx.tar
+
+
+ssh -p 5544 $server docker-compose up
