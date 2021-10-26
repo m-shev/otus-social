0c69c2490b61c2026b357c809190f2c4074b4834
mshev <mshev@list.ru>
2021-10-26 20:56:53 +0300

    minor fixes

--- script/build.sh
+++ script/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
-#bash -c "cd .. && docker build -f nginx/Dockerfile --no-cache -t sigma-nginx:latest ."
-#docker save -o ./images/sigma-nginx.tar sigma-nginx:latest
+bash -c "cd .. && docker build -f nginx/Dockerfile --no-cache -t sigma-nginx:latest ."
+docker save -o ./images/sigma-nginx.tar sigma-nginx:latest
 
 ## back
-bash -c "cd ../back && docker build -t sigma-social:latest ."
-docker save -o ./images/sigma-social.tar sigma-social:latest
+#bash -c "cd ../back && docker build -t sigma-social:latest ."
+#docker save -o ./images/sigma-social.tar sigma-social:latest
 
 ## backend
-bash -c "cd ../db && bash ./build.sh"
-docker save -o ./images/sigma-social-db.tar sigma-social-db:latest
+#bash -c "cd ../db && bash ./build.sh"
+#docker save -o ./images/sigma-social-db.tar sigma-social-db:latest
--- script/deploy.sh
+++ script/deploy.sh
@@ -5,12 +5,13 @@ server=sigma@77.244.214.34
 imagesPath=/home/sigma/images
 
 scp -P 5544 ./images/sigma-nginx.tar $server:$imagesPath
-scp -P 5544 ./images/sigma-social.tar $server:$imagesPath
-scp -P 5544 ./images/sigma-social-db.tar $server:$imagesPath
+#scp -P 5544 ./images/sigma-social.tar $server:$imagesPath
+#scp -P 5544 ./images/sigma-social-db.tar $server:$imagesPath
+scp -P 5544 ./docker-compose.yml $server:/home/sigma
 
 ssh -p 5544 $server docker load -i $imagesPath/sigma-nginx.tar
 ssh -p 5544 $server docker load -i $imagesPath/sigma-social.tar
-ssh -p 5544 $server docker load -i $imagesPath/sigma-nginx.tar
+ssh -p 5544 $server docker load -i $imagesPath/sigma-social-db.tar
 
 
 ssh -p 5544 $server docker-compose up
