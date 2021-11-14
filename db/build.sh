69129a2c59b709b654f87e764bbe4aee1ba0c704
mshev <mshev@list.ru>
2021-11-14 18:55:42 +0300

    HW3 WIP

--- db/build.sh
+++ db/build.sh
@@ -2,8 +2,44 @@
 set -e
 source ".msql"
 
+mycnf1="[mysqld]
+# ID slave
+server-id = 2
+
+# Путь к relay логу
+relay-log = /var/log/mysql/mysql-relay-bin.log
+
+# Путь к bin логу на Мастере
+log_bin = /var/log/mysql/mysql-bin.log
+
+# название Вашей базы данных, которая будет реплицироваться
+binlog_do_db=socialdb"
+
+mycnf2="[mysqld]
+       # ID slave
+       server-id = 3
+
+       # Путь к relay логу
+       relay-log = /var/log/mysql/mysql-relay-bin.log
+
+       # Путь к bin логу на Мастере
+       log_bin = /var/log/mysql/mysql-bin.log
+
+       # название Вашей базы данных, которая будет реплицироваться
+       binlog_do_db=socialdb"
+
 ## build master
 #docker build --no-cache --build-arg sec="$R" -t sigma-social-db:latest .
 
 ## build slave1
-docker build --no-cache --build-arg sec="$R" -f ./slave1/Dockerfile -t sigma-social-db-replica-1:latest .
+#rm ./slave1/.my.cnf
+#echo "$mycnf1" >> ./slave1/.my.cnf
+#docker build --no-cache --build-arg sec="$R" -f ./slave1/Dockerfile -t sigma-social-db-replica-1:latest .
+
+# build slave2
+#rm ./slave1/.my.cnf
+#echo "$mycnf2" >> ./slave1/.my.cnf
+#docker build --no-cache --build-arg sec="$R" -f ./slave1/Dockerfile -t sigma-social-db-replica-2:latest .
+
+#build proxysql
+docker build --no-cache -f ./proxysql/Dockerfile -t sigma-social-proxysql:latest .
--- db/slave1/docker-entrypoint-initdb.d/000001.sh
+++ db/slave1/docker-entrypoint-initdb.d/000001.sh
@@ -5,4 +5,5 @@ source "../.msql"
 mysql -u root -p"$R" -e "create database socialdb";
 #mysql -u root -p"$R" socialdb < socialdb.sql;
 #mysql -u root -p"$R" -e "CHANGE MASTER TO MASTER_HOST='db', MASTER_USER='slave_user', MASTER_PASSWORD='"$RPL"',  MASTER_LOG_FILE = 'mysql-bin.000001', MASTER_LOG_POS = 156;"
-mysql -u root -p"$R" -e "CHANGE MASTER TO MASTER_HOST='db', MASTER_USER='slave_user', MASTER_PASSWORD='"$RPL"', MASTER_LOG_FILE = 'mysql-bin.000001', MASTER_LOG_POS = 4;"
+mysql -u root -p"$R" socialdb < socialdb.sql;
+mysql -u root -p"$R" -e "CHANGE MASTER TO MASTER_HOST='db', MASTER_USER='slave_user', MASTER_PASSWORD='"$RPL"', MASTER_LOG_FILE = 'mysql-bin.000005', MASTER_LOG_POS = 156;"
